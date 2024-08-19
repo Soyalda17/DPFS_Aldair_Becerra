f0771191dd1c5331bebbdd71dbc8b0fe0b9ad2d7
Yaneika <yaneikanieto@gmail.com>
2024-08-18 19:26:27 -0500

    terminando el home

---
+++ scripts.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const carouselInner = document.querySelector('.carousel-inner');
+  const images = carouselInner.querySelectorAll('img');
+  const imageWidth = images[0].clientWidth;
+  const visibleImages = 3;
+  let index = 0;
+
+  function nextImage() {
+    index++;
+    if (index >= (images.length - visibleImages + 1)) {
+      index = 0;
+    }
+    updateCarousel();
+  }
+
+  function updateCarousel() {
+    const offset = -index * imageWidth;
+    carouselInner.style.transform = `translateX(${offset}px)`;
+  }
+
+  setInterval(nextImage, 5000); // Cambia de imagen cada 5 segundos
+});
